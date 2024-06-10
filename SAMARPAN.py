406bdc2e52abbf7d3f2dd6c50c252f6da8abc760
NOBITAxGM <songgm39@gmail.com>
2024-06-11 03:47:56 +0545

    Update SAMARPAN.py

--- SAMARPAN.py
+++ SAMARPAN.py
@@ -1,1 +1,23 @@
-import samarpan1_enc
+#####apni 
+ #coding=utf-8
+import os, sys, platform
+ 
+os.system('git pull')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('git pull')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    
+        import samarpan1_enc
+    
+ 
+elif bit == '32bit':
+    
+        import samarpan1_enc
+    
